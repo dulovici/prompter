c4b8f35480bc40ce651c781e19af4d6d8ec059be
Igor Dulovic <igorx24@gmail.com>
2023-11-28 11:30:15 +0100

    create new post action

--- app/page.jsx
+++ app/page.jsx
@@ -6,11 +6,11 @@ const Home = () => {
       <h1 className="head_text text-center">
         Discover & Share
         <br className="max-md:hidden" />
-        <span className="orange_gradient text-center">Ai-Powered Promts</span>
+        <span className="orange_gradient text-center">Ai-Powered Prompts</span>
       </h1>
 
       <p className="desc text-center">
-        Promptopia is an open-source AI prompting tool for modern world to
+        Prompter is an open-source AI prompting tool for modern world to
         discover, create and share creative prompts
       </p>
 
